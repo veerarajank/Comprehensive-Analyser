4f84cb593c299c11a78da06ca1cbc0a302ffaef1
Veerarajan <v.karunanithi@transformuk.com>
2024-06-07 18:10:12 +0100

    password functionality

--- MultiAgentCompanyDebate.py
+++ MultiAgentCompanyDebate.py
@@ -7,6 +7,7 @@ import os
 from langgraph.graph import StateGraph, END
 from typing import Dict, TypedDict, Optional
 import streamlit as st
+import hmac
 
 load_dotenv()
 # Create the Groq client
@@ -90,6 +91,33 @@ workflow.add_edge("expert", END)
 
 app = workflow.compile()
 inputs = {"history": "", "company": company, "prop_sentiment": "", "prop_technical": "", "prop_fundamental": "", "prop_expert": ""}
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], os.getenv("password")):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 st.header("Comprehensive Analysis Partner")
 st.write("Hello! Good day! We are team of specialists, Sentiment Analyser (S) , Technical Analyser (T), Fundamental Analyser(F) and Expert(E) in the field of financial markets.  Which Company do you want to analyze ? Please provide the respective code for the company. For example for Apple Inc, please provide as AAPL")
